5aa3585e4784a0cefea69f274edfd0bfb1956cc2
Raymond Richter <73314864+GandalphSnaxx@users.noreply.github.com>
2025-02-11 16:37:33 -0700

    Updated configuration

--- build.bat
+++ build.bat
@@ -2,10 +2,14 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-SET includes=/Isrc /I%VULKAN_SDK%/Include
-SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib user32.lib
+set GLFW_include=Libraries\glfw-3.4.bin.WIN64\include
+set GLFW_lib=Libraries\glfw-3.4.bin.WIN64\lib-vc2022
+set GLM_include=Libraries\glm
+
+SET includes=/Isrc /I%VULKAN_SDK%/Include /I%GLFW_include% /I%GLM_include%
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib user32.lib /LIBPATH:%GLFW_lib% glfw3.lib
 SET defines=/D DEBUG /D WINDOWS_BUILD
 
 echo "Building main..."
 
-cl /EHsc /Z7 /Fe"main" %includes% %defines% src/platform/win_platform.cpp %links%
+cl /EHsc /Z7 /Fe"main" %includes% %defines% src/hello_triangle.cpp %links%
